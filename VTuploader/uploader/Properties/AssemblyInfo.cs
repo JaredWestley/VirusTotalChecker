d05be6935bc0af4c02ab3830c64f33cda9db1a5e
JaredWestley <105741779+JaredWestley@users.noreply.github.com>
2022-08-14 13:39:54 +0100

    Add files via upload

---
+++ VTuploader/uploader/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("uploader")]
+[assembly: AssemblyDescription("Open-Source Winforms application for uploading files to VirusTotal ")]
+#if (DEBUG)
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Samuel Tulach")]
+[assembly: AssemblyProduct("VirusTotalUploader")]
+[assembly: AssemblyCopyright("Copyright © 2021 Samuel Tulach")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b5329de8-8cd7-4b08-9e87-26778404c7a5")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
